dbea2b604cef005a38b32141ce6b3302bc6c788b
Kivanc Ozisik <kivanc.ozisik@gmail.com>
2026-02-14 12:15:13 +0300

    learn matrix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ df=pd.read_csv("items.txt",sep=';')
 
 #print(df["meaning"][14])
 #print(len(df["word"]))
-
+SIZE_DICT=len(df["word"])
 num=random.randint(0,len(df["word"])-1)
 
 c_response=0
@@ -13,14 +13,27 @@ w_response=0
 num_old=[]
 response=""
 
-while response!="exit":
+learn_matrix=pd.DataFrame(data={"word":[''],"c_response":[0],"w_response":[0],"w/c_ratio":[0]})
+print(learn_matrix)
+while response!="exit": # sonda wrong yazısını düzeltmem gerek.
     print(f"What is the word for the following: {df['meaning'][num]}")
     response=input("")
     if response==df["word"][num]:
-        c_response+=1
+        if df["word"][num] in learn_matrix["word"]:
+            learn_matrix.loc[learn_matrix["word"]==df["word"][num],"c_response"]+=1
+        else:
+            learn_matrix.loc[len(learn_matrix)]=[df["word"][num],0,0,0]
+            learn_matrix.loc[learn_matrix["word"] == df["word"][num], "c_response"] += 1
         print("Correct!")
     else:
-        w_response+=1
+        if df["word"][num] in learn_matrix["word"]:
+            learn_matrix.loc[learn_matrix["word"]==df["word"][num],"w_response"]+=1
+        else:
+            learn_matrix.loc[len(learn_matrix)]=[str(df["word"][num]),0,0,0]
+            learn_matrix.loc[learn_matrix["word"] == df["word"][num], "w_response"] += 1
         print("Wrong")
+    learn_matrix.loc[learn_matrix["word"] == df["word"][num], "w/c_ratio"] =learn_matrix.loc[learn_matrix["word"] == df["word"][num], "w_response"]/(1+learn_matrix.loc[learn_matrix["word"] == df["word"][num], "c_response"])
     num_old.append(num)
     num=random.randint(0,len(df["word"])-1)
+print(learn_matrix)
+print(num_old)
