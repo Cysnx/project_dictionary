0ea61a08c140fd1e572fdecaa8acd58675384e00
Kivanc Ozisik <kivanc.ozisik@gmail.com>
2026-02-19 16:17:29 +0300

    modular programming started

---
+++ learning_method.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import pandas as pd
+
+def power_law(c_response,w_response,datetime_last_c_response): ## bunu çalışmam gerek
+    k = w_response / (1 + c_response)
+    today = datetime.now()
+
+    if (
+        datetime_last_c_response is None
+        or datetime_last_c_response == ''
+        or pd.isna(datetime_last_c_response)
+        or datetime_last_c_response == 'N/A'
+    ):
+        return 0.0
+
+    if isinstance(datetime_last_c_response, str):
+        last_c = datetime.fromisoformat(datetime_last_c_response)
+    else:
+        last_c = pd.to_datetime(datetime_last_c_response).to_pydatetime()
+
+    delta = (today - last_c).total_seconds()/86400
+
+    return round((1 + delta) ** (-k),4)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import random
 import os
-import time
 from datetime import datetime
+from learning_method import power_law
 
 df=pd.read_csv("items.txt",sep=';')
 
@@ -16,29 +16,6 @@ c_response=0
 w_response=0
 num_old=[]
 response=""
-
-def power_law(c_response,w_response,datetime_last_c_response): ## bunu çalışmam gerek
-    k = w_response / (1 + c_response)
-    today = datetime.now()
-
-    if (
-        datetime_last_c_response is None
-        or datetime_last_c_response == ''
-        or pd.isna(datetime_last_c_response)
-        or datetime_last_c_response == 'N/A'
-    ):
-        return 0.0
-
-    if isinstance(datetime_last_c_response, str):
-        last_c = datetime.fromisoformat(datetime_last_c_response)
-    else:
-        last_c = pd.to_datetime(datetime_last_c_response).to_pydatetime()
-
-    delta = (today - last_c).total_seconds()/86400
-
-    return round((1 + delta) ** (-k),4)
-
-
 
 if os.path.exists("learn_matrix.txt"):
     learn_matrix=pd.read_csv("learn_matrix.txt", sep=';')
